c2d778af5087c49594612f48c25787003f17bb11
Arfey <misha.gavela@dataroot.co>
2016-06-16 01:14:26 +0300

    Cleared git

--- Portfolio/site_Dataroot/project_portfolio/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MyUser, Projects, Link
-from django.contrib.auth.models import User, Group
-
-
-admin.site.register(MyUser)
-admin.site.unregister(Group)
-
--- Portfolio/site_Dataroot/project_portfolio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProjectPortfolioConfig(AppConfig):
-    name = 'project_portfolio'
